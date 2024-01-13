9919b079abc70c84b43a5cfc4484f5fcf93a1bb4
chrisouko <oukodulo@gmail.com>
2024-01-13 22:53:39 +0300

    Completed assignment

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -12,3 +13,8 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
+
+    def to_dict(self):
+        return {'id': self.id, 'body': self.body, 'username': self.username}
